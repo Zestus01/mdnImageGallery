755c926cff273b527d9e9b4590a98528ca435d92
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-09-18 16:59:19 +0000

    :   gallery-start/style.css

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,8 +1,12 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
+
+const thumbBar2 = document.querySelector('.thumb-bar2');
+const displayedImage2 = document.querySelector('.displayed-img2');
+const overlay2 = document.querySelector('.overlay2');
+//const btn2 = document.querySelector('button2');
 
 /* Declaring the array of image filenames */
 const imageStr = [
@@ -36,3 +40,45 @@ for(image of imageStr){
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    let btnCheck = btn.getAttribute('class');
+    if(btnCheck === 'dark'){
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0, 0, 0, 0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0, 0, 0, 0)';
+    }
+});
+
+// Setting up the second image gallery
+const imageStr2 = [
+    './image2/babyYoda.jpg',
+    './image2/Corgi_train.jpg',
+    './image2/Dembois.jpg',
+    './image2/snowball-in-hell.jpg',
+    './image2/Waveofthefuture.jpg',
+];
+
+const altStr2 = [
+    'Baby Yoda and the Mandolorian',
+    'Corgis on a train',
+    'Daffodils',
+    'Egyptian Hieroglyphs',
+    'A nice brown butterfly',
+];
+
+let index2 = 1;
+for(image of imageStr2){
+    const newImage2 = document.createElement('img');
+    newImage2.setAttribute('src', image);
+    newImage2.setAttribute('alt', altStr2[index]);
+    thumbBar2.appendChild(newImage2);
+    index++;
+    newImage2.addEventListener('click', e2 => {
+        displayedImage2.src = e2.target.src;
+        displayedImage2.alt = e2.target.alt;
+    })
+}
